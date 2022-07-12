e94f8f431870cf6a95a438204c493a166b244c1b
Daniel Alves <danalvescc@gmail.com>
2022-07-11 21:34:14 -0300

    Initial Commit

---
+++ BankerUITests/BankerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BankerUITestsLaunchTests.swift
+//  BankerUITests
+//
+//  Created by Daniel  Alves Barreto on 11/07/22.
+//
+
+import XCTest
+
+class BankerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
