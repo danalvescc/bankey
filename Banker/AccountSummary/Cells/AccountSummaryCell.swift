0cfd45ba1b519d2780539edf1c5c5c3fe8d36fac
Daniel Alves <danalvescc@gmail.com>
2022-11-21 20:54:37 -0300

    feat: create tableviewcell

---
+++ Banker/AccountSummary/Cells/AccountSummaryCell.swift
@@ -0,0 +1,115 @@
+//
+//  AccountSummaryCell.swift
+//  Banker
+//
+//  Created by Daniel  Alves Barreto on 21/11/22.
+//
+
+import Foundation
+import UIKit
+
+class AccountSummaryCell: UITableViewCell {
+    
+    private lazy var typeLable: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .caption1)
+        label.adjustsFontForContentSizeCategory = true
+        label.text = "test"
+        return label
+    }()
+    
+    private lazy var underlineView: UIView = {
+        var view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = appColor
+        return view
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.text = "Account Name"
+        return label
+    }()
+    
+    private lazy var balanceStackView: UIStackView = {
+        var stackview = UIStackView()
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        stackview.axis = .vertical
+        stackview.spacing = 0
+        return stackview
+    }()
+    
+    private lazy var balanceLabel: UILabel = {
+        var label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textAlignment = .right
+        label.text = "Some Label"
+        return label
+    }()
+    
+    private lazy var balanceAmountLabel: UILabel = {
+        var label = UILabel()
+        label.text = "$924.123,00"
+        return label
+    }()
+    
+    private lazy var chevronImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        let chevronImage = UIImage(systemName: "chevron.right")!.withTintColor(appColor, renderingMode: .alwaysOriginal)
+        imageView.image = chevronImage
+        return imageView
+    }()
+    
+    static var reuseID = "AccountSummaryCell"
+    static var rowHeight: CGFloat = 100
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+extension AccountSummaryCell {
+    private func setup(){
+        setupBalanceStackView()
+        contentView.addSubview(typeLable)
+        contentView.addSubview(underlineView)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(balanceStackView)
+        contentView.addSubview(chevronImageView)
+    }
+    
+    private func setupBalanceStackView(){
+        balanceStackView.addArrangedSubview(balanceLabel)
+        balanceStackView.addArrangedSubview(balanceAmountLabel)
+    }
+    
+    private func layout(){
+        NSLayoutConstraint.activate([
+            typeLable.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2),
+            typeLable.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            underlineView.topAnchor.constraint(equalToSystemSpacingBelow: typeLable.bottomAnchor, multiplier: 1),
+            underlineView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            underlineView.widthAnchor.constraint(equalToConstant: 60),
+            underlineView.heightAnchor.constraint(equalToConstant: 4),
+            nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: underlineView.bottomAnchor, multiplier: 2),
+            nameLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            balanceStackView.topAnchor.constraint(equalToSystemSpacingBelow: underlineView.bottomAnchor, multiplier: 0),
+            balanceStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: nameLabel.trailingAnchor, multiplier: 4),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: balanceStackView.trailingAnchor, multiplier: 4),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: chevronImageView.trailingAnchor, multiplier: 1),
+            chevronImageView.topAnchor.constraint(equalToSystemSpacingBelow: underlineView.bottomAnchor, multiplier: 1)
+        ])
+    }
+}
