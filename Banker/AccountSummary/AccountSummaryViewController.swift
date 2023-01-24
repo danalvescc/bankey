97ffee884ae3bd2ae2acbb860bea7af96228da36
Daniel Alves <danalvescc@gmail.com>
2023-01-23 22:21:37 -0300

    feat: add DispatchGroup on fetch account data

--- Banker/AccountSummary/AccountSummaryViewController.swift
+++ Banker/AccountSummary/AccountSummaryViewController.swift
@@ -37,7 +37,7 @@ extension AccountSummaryViewController {
         setupTableView()
         setupTableHeaderView()
         setupNavigationBar()
-        fetchDataAndLoadViews()
+        fetchData()
     }
     
     private func setupNavigationBar(){
@@ -103,28 +103,35 @@ extension AccountSummaryViewController {
 
 // MARK: Networking
 extension AccountSummaryViewController {
-    func fetchDataAndLoadViews() {
+    func fetchData() {
+        let group = DispatchGroup()
+        group.enter()
         fetchProfile(forUserId: "1") {result in
             switch result {
             case .success(let profile):
                 self.profile = profile
                 self.configureTableHeaderView(with: profile)
-                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
         }
         
+        group.enter()
         fetchAccounts(forUserId: "1") {result in
             switch result {
             case .success(let accounts):
                 self.accounts = accounts
                 self.configureTableCells(with: accounts)
-                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
             
+        }
+        
+        group.notify(queue: .main) {
+            self.tableView.reloadData()
         }
     }
     
